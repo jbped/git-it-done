1401cf460e69a264c5cfd8dcfc9c71da99e323e7
jbped <pedigojacob@gmail.com>
2021-05-25 12:27:09 -0600

    added link to repo issue board for repos with 30+ issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.getElementById("issues-container");
+var limitWarningEl = document.getElementById("limit-warning")
 
 var getRepoIssues = function(repo) {
     // Set apiUrl, use passed through value of repo to dynamically alter it repo format === "user/repo"
@@ -8,6 +9,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data)
+            
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+               displayWarning(repo);
+            }
             });
         } else {
             alert("There was a problem with your request!")
@@ -18,35 +24,46 @@ var getRepoIssues = function(repo) {
 var displayIssues = function(issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "No issues were found within the specified repo"
-    } else {
-        for (var i = 0; i < issues.length; i++) {
-            var issueEl = document.createElement ("a");
-                issueEl.classList = "list-item flex-row justify-space-between align-center";
-                issueEl.setAttribute("href", issues[i].html_url);
-                issueEl.setAttribute("target", "_blank");
-            
-            // Create span to hold info
-            var titleEl = document.createElement("span")
-                titleEl.textContent = issues[i].title;
-    
-            // Append to parent
-            issueEl.appendChild(titleEl);
-    
-            // create a type element
-            var typeEl = document.createElement("span");
-    
-            // verify that issue is an issue and not a pull request
-            if (issues[i].pull_request) {
-                typeEl.textContent = "(Pull Request)"
-            }  else {
-                typeEl.textContent = "(Issue)"
-            };
-    
-            issueEl.appendChild(typeEl);
-        }
+        return;
+    }
+    for (var i = 0; i < issues.length; i++) {
+        var issueEl = document.createElement ("a");
+            issueEl.classList = "list-item flex-row justify-space-between align-center";
+            issueEl.setAttribute("href", issues[i].html_url);
+            issueEl.setAttribute("target", "_blank");
+        
+        // Create span to hold info
+        var titleEl = document.createElement("span")
+            titleEl.textContent = issues[i].title;
+
+        // Append to parent
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // verify that issue is an issue and not a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull Request)"
+        }  else {
+            typeEl.textContent = "(Issue)"
+        };
+
+        issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
-
 }
 
-getRepoIssues("jbped/brain-picker")
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+        linkEl.textContent = "the issues board on GitHub.com"
+        linkEl.setAttribute("href","https://github.com/" + repo + "/issues")
+        linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("angular/angular")
